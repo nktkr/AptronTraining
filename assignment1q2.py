56d0995d82b5464258390fdc6ba3d67c13b2b7cd
nktkr <100942921+nktkr@users.noreply.github.com>
2022-07-24 23:50:59 +0530

    assignment2 done

--- assignment1q2.py
+++ assignment1q2.py
@@ -1,12 +1,13 @@
 #Question2:
 #Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each
 #digit of the number is an even number.
-l=1000
-j=3000
+
 for i in range (1000,3000):
     k=str(i)
-    k0=k[0]
-    k1=k[1]
-    k2=
-    if()
-    print(i)
+    k0=int(k[0])%2
+    k1=int(k[1])%2
+    k2=int(k[2])%2
+    k3=int(k[3])%2
+    sum=k0+k1+k2+k3
+    if sum==0:
+        print(i)
