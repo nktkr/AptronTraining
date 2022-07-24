062b22a796a991af162a89b8118534b54d1e617e
nktkr <100942921+nktkr@users.noreply.github.com>
2022-07-24 23:42:25 +0530

    Initial commit

---
+++ assignment 1 q1.py
@@ -0,0 +1,4 @@
+#Question1:
+#Write a program that accepts a sentence and calculate the number of letters and digits.
+sentence = input('enter the sentence: ')
+for
---
+++ assignment1q2.py
@@ -0,0 +1,12 @@
+#Question2:
+#Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each
+#digit of the number is an even number.
+l=1000
+j=3000
+for i in range (1000,3000):
+    k=str(i)
+    k0=k[0]
+    k1=k[1]
+    k2=
+    if()
+    print(i)
