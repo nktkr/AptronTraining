a3b1a7b4b7d066e0f13517c1f4fd9dc1a96f7849
nktkr <100942921+nktkr@users.noreply.github.com>
2022-07-25 01:21:02 +0530

    assignment7 done

--- Assignment_1/assignment1q1.py
+++
@@ -1,3 +0,0 @@
-#Question1:
-#Write a program that accepts a sentence and calculate the number of letters and digits.
-i wanna use the concept of dictionary in this
--- Assignment_1/assignment1q5.py
+++
@@ -1,6 +0,0 @@
-'''Question5:
-Write a program to print multiplication table of a given number.'''
-num=int(input('hello there\nenter the no. of which u want to print the table:'))
-print('the table of', num , 'goes like this')
-for i in range (1,11):
-    print(i*num)
